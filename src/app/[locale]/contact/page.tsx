affd457c3428c4d19edd82bd72f539db7d9460ff
khanhvinhnguyen <khanhvinh.nguyen.0628@gmail.com>
2024-08-05 11:22:06 +0700

    fix: contact css

--- src/app/[locale]/contact/page.tsx
+++ src/app/[locale]/contact/page.tsx
@@ -58,6 +58,7 @@ const ContactPage = () => {
             </a>
           </div>
           <iframe
+            className="map"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1957.3612563319764!2d106.61448259999999!3d11.134032!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3174cd1028f9c937%3A0xc438142dbf416a24!2z4bqobSB0aOG7sWMgY2hheSBEaeG7h3UgVGhp4buHbg!5e0!3m2!1svi!2s!4v1722583100981!5m2!1svi!2s"
             width="800"
             height="450"
